3cad2a620d9540dd8e200a7e5f773374771564b6
Mourjo Sen <hello@mourjo.me>
2024-05-02 22:37:25 +0200

    Add ACM filter

--- render_images.sh
+++ render_images.sh
@@ -3,12 +3,15 @@
 ENDCOLOR="\e[0m"
 BLUE="\e[34m"
 
-FILES=$(git diff origin/master --name-only | rg puml$)
+
+# diff filter https://git-scm.com/docs/git-diff#Documentation/git-diff.txt---diff-filterACDMRTUXB82308203
+# Select only files that are Added (A), Copied (C), Modified (M)
+FILES=$(git diff origin/master --diff-filter=ACM --name-only | rg puml$)
 
 echo -e "Rendering: ${BLUE}\n$FILES\n\n${ENDCOLOR}"
 
-git diff origin/master --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar ~/software/plantuml-gplv2-1.2024.4.jar -o out -tsvg &
-git diff origin/master --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar ~/software/plantuml-gplv2-1.2024.4.jar -o out -tpng &
+git diff origin/master --name-only --diff-filter=ACM  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar ~/software/plantuml-gplv2-1.2024.4.jar -o out -tsvg &
+git diff origin/master --name-only --diff-filter=ACM  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar ~/software/plantuml-gplv2-1.2024.4.jar -o out -tpng &
 
 wait
 git add */out
