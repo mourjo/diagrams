592685a750363bdc0ee6d0b6f7a037f97abe36a0
Mourjo Sen <hello@mourjo.me>
2024-05-02 08:38:33 +0200

    Rename branch

--- render_images.sh
+++ render_images.sh
@@ -7,8 +7,8 @@ FILES=$(git diff origin/main --name-only | rg puml$)
 
 echo -e "Rendering: ${BLUE}\n$FILES\n\n${ENDCOLOR}"
 
-git diff origin/main --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar /Users/mourjo/software/plantuml-gplv2-1.2024.4.jar -o out -tsvg &
-git diff origin/main --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar /Users/mourjo/software/plantuml-gplv2-1.2024.4.jar -o out -tpng &
+git diff origin/master --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar /Users/mourjo/software/plantuml-gplv2-1.2024.4.jar -o out -tsvg &
+git diff origin/master --name-only  | rg puml$ | xargs -I{} echo "'{}'" | xargs java -jar /Users/mourjo/software/plantuml-gplv2-1.2024.4.jar -o out -tpng &
 
 wait
 git add */out
